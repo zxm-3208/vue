d9c4e20fcdf4bfc25825ec1f6c62489d68d802f8
zxm-3208 <zhou-xueming@foxmail.com>
2024-05-15 22:04:48 +0800

    关注列表完成，TODO, 列表里执行关注动作

--- src/views/me/UserFollowList.vue
+++ src/views/me/UserFollowList.vue
@@ -7,68 +7,16 @@
 				</div>
 				<!-- 消息列表 -->
 				<div class="msg-list-box">
-					<div class="msg-list">
-						<img src="../../../public/images/head2.jpg" alt="">
+					<div class="msg-list" v-for="(item,index) in userIdList" :key="index">
+						<img :src="iconList[index]" alt="">
 						<div class="user-des">
 							<div class="top">
-								<span>抖音小助手</span>
-								<span>12：20</span>
+								<span>{{nameList[index]}}</span>
+								<span v-if="followFlag==1"  @click="handleFollow" >已关注</span>
+								<span v-if="followFlag==0" @click="handleFollow" >关注</span>
 							</div>
 							<div class="top top-msg">
-								<span>抖音安全课堂</span>
-								<span class="no-see"></span>
-							</div>
-						</div>
-					</div>
-					<div class="msg-list">
-						<img src="../../../public/images/head1.jpg" alt="">
-						<div class="user-des">
-							<div class="top">
-								<span>抖音小助手</span>
-								<span>2：20</span>
-							</div>
-							<div class="top top-msg">
-								<span>在干嘛？</span>
-								<span class="no-see"></span>
-							</div>
-						</div>
-					</div>
-					<div class="msg-list">
-						<img src="../../../public/images/head1.jpg" alt="">
-						<div class="user-des">
-							<div class="top">
-								<span>路人甲</span>
-								<span>2：22</span>
-							</div>
-							<div class="top top-msg">
-								<span>在干嘛？</span>
-								<span class="no-see"></span>
-							</div>
-						</div>
-					</div>
-					<div class="msg-list">
-						<img src="../../../public/images/head1.jpg" alt="">
-						<div class="user-des">
-							<div class="top">
-								<span>路人甲</span>
-								<span>2：22</span>
-							</div>
-							<div class="top top-msg">
-								<span>在干嘛？</span>
-								<span class="no-see"></span>
-							</div>
-						</div>
-					</div>
-					<div class="msg-list">
-						<img src="../../../public/images/head1.jpg" alt="">
-						<div class="user-des">
-							<div class="top">
-								<span>路人甲</span>
-								<span>2：22</span>
-							</div>
-							<div class="top top-msg">
-								<span>在干嘛？</span>
-								<span class="no-see"></span>
+								<span>{{introductionList[index]}}</span>
 							</div>
 						</div>
 					</div>
@@ -79,15 +27,83 @@
 </template>
 <script>
 	import Myheader from '../../components/header/Myheader.vue'
+	import axios from 'axios'
 	export default{
+		data(){
+			return {
+				iconUrl: '',
+
+				lastId: 0,
+				offset: 0,
+
+				userIdList: [],
+				iconList: [],
+				nameList: [],
+				introductionList: [],
+
+				followFlag: 1,
+			}
+		},
 		name:"",
 		components:{
 			Myheader
+		},
+		created(){
+			this.initFollow();
 		},
 		methods:{
 			toBack(){
 				this.$router.push('/me')
 			},	
+			async initFollow(){
+				this.userId = localStorage.getItem('userId');
+				this.lastId = Date.parse(new Date());
+				let res = await axios.post('http://localhost:8020/douyin_user/followList/getFollowList',{
+					"userId": this.userId,
+					"lastId": this.lastId,
+					"offset": this.offset,
+				},
+				{
+					headers: {
+						'Authorization': 'Bearer ' + localStorage.getItem('authorization')
+					}
+				})
+				console.info("followList:",res);
+				if(res.data.code=="200"){
+					this.userIdList = res.data.data.userIdList
+					this.iconList = res.data.data.iconList
+					this.nameList = res.data.data.nameList
+					this.introductionList = res.data.data.introductionList
+					console.info(this.userIdList)
+					console.info(this.iconList)
+					console.info(this.nameList)
+					console.info(this.introductionList)
+					for(var i = 0; i < this.userIdList.length; i++){
+						if(this.iconList[i]==null || this.iconList[i]==''){
+							this.iconList[i] = 'https://p.qqan.com/up/2018-3/15217745038903395.jpg'
+						}
+					}
+					console.info(this.iconList)
+				}
+				else{
+					this.$toast('获取初始化信息失败！')
+				}
+			},
+			// // todo
+			// async changeFollow(){
+			// 	try{
+			// 		let res = await axios.post('http://localhost:8020/douyin_user/follow/authorFollow',{
+			// 			"userId": this.userId, 
+			// 			"authorId": this.authorIdList[this.mediaindex],
+			// 			"isFollow": this.isFollow,
+			// 		})
+			// 		console.info("changeFollow:",res);
+			// 	}
+			// 	catch(err){
+			// 		console.error(err);
+			// 	}
+			// 	this.getInitFollow();
+			// },
 		}
 	}
 </script>
